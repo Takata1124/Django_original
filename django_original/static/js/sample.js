89584b466b80c298c8219a76d5db891fef2a081a
takata <skkyosk1124@gmail.com>
2022-02-04 05:58:54 +0900

    add commit

--- django_original/static/js/sample.js
+++ django_original/static/js/sample.js
@@ -1,6 +1,16 @@
-function total(price) {
-    const tax = 0.1;
-    return price + price * tax
-}
+// function total(price) {
+//     const tax = 0.1;
+//     return price + price * tax
+// }
 
-document.getElementById('output').textContent = 'コーヒーメーカーの値段は' + total(8000) + '円です';
+// document.getElementById('output').textContent = 'コーヒーメーカーの値段は' + total(8000) + '円です';
+
+// $(function(){
+// 	$("button").click(function(){
+// 		// $(this)
+// 		$(this).html("Click").css("background", "#000033");
+// 	});
+// });
+
+
+
