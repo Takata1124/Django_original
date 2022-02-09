f04721604ff2a80693c428fee6cd91a9d4f74b0d
takata1124 <skkyosk1124@gmail.com>
2022-02-09 12:12:42 +0000

    add ssh edit

--- django_original/settings.py
+++ django_original/settings.py
@@ -21,7 +21,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, 'secrets', '.env'))
+env.read_env(os.path.join(BASE_DIR, '.env'))
 DEBUG = env('DEBUG')
 
 with open(os.path.join(BASE_DIR, 'secrets', 'secret_dev.yaml')) as file:
@@ -33,12 +33,12 @@ with open(os.path.join(BASE_DIR, 'secrets', 'secret_dev.yaml')) as file:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.list('ALLOWED_HOSTS')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['35.74.175.166']
 
 # Application definition
 
