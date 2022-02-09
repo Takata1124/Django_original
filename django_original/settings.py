3b50a8eb84caa315a1941ecfb4680404443cdcca
takata <skkyosk1124@gmail.com>
2022-02-09 17:15:37 +0900

    editing

--- django_original/settings.py
+++ django_original/settings.py
@@ -33,12 +33,12 @@ with open(os.path.join(BASE_DIR, 'secrets', 'secret_dev.yaml')) as file:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = env.list('ALLOWED_HOSTS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Application definition
 
@@ -87,10 +87,7 @@ WSGI_APPLICATION = 'django_original.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
